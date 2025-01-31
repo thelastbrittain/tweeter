faaf1e2df3e551606766dfa03a2524c9c5258d7d
thelastbrittain <benjaminpbrittain@gmail.com>
2025-01-30 23:39:44 -0700

    Initial commit

---
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+interface Props {
+  alias: String,
+  setAlias: (newAlias: string) => void,
+  password: String,
+  setPassword: (newPassword: string) => void,
+  checkSubmitButtonStatus: Function,
+  doLogin: Function
+}
+
+const AuthenticationFields = (props: Props) => {
+
+
+    const loginOnEnter = (event: React.KeyboardEvent<HTMLElement>) => {
+      if (event.key == "Enter" && !props.checkSubmitButtonStatus()) {
+        props.doLogin();
+      }
+    };
+
+    // const checkSubmitButtonStatus = (): boolean => {
+    //   return !props.alias || !props.password;
+    // };
+
+    // ask about this method as well. It's different for both 
+    // const checkSubmitButtonStatus = (): boolean => {
+    //   return !alias || !password;
+    // }; 
+
+    return (
+        <>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            size={50}
+            id="aliasInput"
+            placeholder="name@example.com"
+            onKeyDown={loginOnEnter}
+            onChange={(event) => props.setAlias(event.target.value)}
+          />
+          <label htmlFor="aliasInput">Alias</label>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="password"
+            className="form-control bottom"
+            id="passwordInput"
+            placeholder="Password"
+            onKeyDown={loginOnEnter}
+            onChange={(event) => props.setPassword(event.target.value)}
+          />
+          <label htmlFor="passwordInput">Password</label>
+        </div>
+        </>
+    )
+}
+
+export default AuthenticationFields;
---
+++ tweeter-web/src/components/mainLayout/StatusItemScroller.tsx
@@ -0,0 +1,139 @@
+import { useContext } from "react";
+import { UserInfoContext } from "../userInfo/UserInfoProvider";
+import { AuthToken, FakeData, Status, User } from "tweeter-shared";
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
+import Post from "../statusItem/Post";
+import useToastListener from "../toaster/ToastListenerHook";
+import StatusItem from "../statusItem/StatusItem";
+
+export const PAGE_SIZE = 10;
+
+interface Props {
+    message: string,
+    loadMore: (
+        authToken: AuthToken,
+        userAlias: string,
+        pageSize: number,
+        lastItem: Status | null
+      ) => Promise<[Status[], boolean]>
+}
+
+const StatusItemScroller = (props: Props) => {
+    const { displayErrorMessage } = useToastListener();
+    const [items, setItems] = useState<Status[]>([]);
+    const [newItems, setNewItems] = useState<Status[]>([]);
+    const [hasMoreItems, setHasMoreItems] = useState(true);
+    const [lastItem, setLastItem] = useState<Status | null>(null);
+    const [changedDisplayedUser, setChangedDisplayedUser] = useState(true);
+
+  const addItems = (newItems: Status[]) =>
+    setNewItems(newItems);
+
+  const { displayedUser, setDisplayedUser, currentUser, authToken } =
+    useContext(UserInfoContext);
+
+  // Initialize the component whenever the displayed user changes
+  useEffect(() => {
+    reset();
+  }, [displayedUser]);
+
+  // Load initial items whenever the displayed user changes. Done in a separate useEffect hook so the changes from reset will be visible.
+  useEffect(() => {
+    if(changedDisplayedUser) {
+      loadMoreItems();
+    }
+  }, [changedDisplayedUser]);
+
+  // Add new items whenever there are new items to add
+  useEffect(() => {
+    if(newItems) {
+      setItems([...items, ...newItems]);
+    }
+  }, [newItems])
+
+  const reset = async () => {
+    setItems([]);
+    setNewItems([]);
+    setLastItem(null);
+    setHasMoreItems(true);
+    setChangedDisplayedUser(true);
+  }
+
+  const loadMoreItems = async () => {
+    try {
+      const [newItems, hasMore] = await props.loadMore(
+        authToken!,
+        displayedUser!.alias,
+        PAGE_SIZE,
+        lastItem
+      );
+
+      setHasMoreItems(hasMore);
+      setLastItem(newItems[newItems.length - 1]);
+      addItems(newItems);
+      setChangedDisplayedUser(false)
+    } catch (error) {
+      displayErrorMessage(
+        `Failed to load ${props.message} items because of exception: ${error}`
+      );
+    }
+  };
+
+  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
+    event.preventDefault();
+
+    try {
+      const alias = extractAlias(event.target.toString());
+
+      const user = await getUser(authToken!, alias);
+
+      if (!!user) {
+        if (currentUser!.equals(user)) {
+          setDisplayedUser(currentUser!);
+        } else {
+          setDisplayedUser(user);
+        }
+      }
+    } catch (error) {
+      displayErrorMessage(`Failed to get user because of exception: ${error}`);
+    }
+  };
+
+  const extractAlias = (value: string): string => {
+    const index = value.indexOf("@");
+    return value.substring(index);
+  };
+
+  const getUser = async (
+    authToken: AuthToken,
+    alias: string
+  ): Promise<User | null> => {
+    // TODO: Replace with the result of calling server
+    return FakeData.instance.findUserByAlias(alias);
+  };
+
+  return (
+    <div className="container px-0 overflow-visible vh-100">
+      <InfiniteScroll
+        className="pr-0 mr-0"
+        dataLength={items.length}
+        next={loadMoreItems}
+        hasMore={hasMoreItems}
+        loader={<h4>Loading...</h4>}
+      >
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="row mb-3 mx-0 px-0 border rounded bg-white"
+          >
+            <StatusItem status={item}/>
+          </div>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+export default StatusItemScroller
